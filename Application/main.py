e5b142f78c20373a6bd83e4910048a2185d8830b
Damir <nabiull2020@gmail.com>
2022-04-08 21:07:51 +0300

    Updated main.py

--- Application/main.py
+++ Application/main.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
 
     app = web.Application()
     app.add_routes(routes)
-    web.run_app(app, host='0.0.0.0', port=8080)
+    web.run_app(app, host='0.0.0.0', port=9090)
 
