49fec356a9f822ca374ef5d7315703f2cc35570c
Damir Nabiullin <51910382+damirnabiull@users.noreply.github.com>
2022-04-08 21:06:58 +0300

    Updated alarm.py

--- Application/alarm.py
+++ Application/alarm.py
@@ -1,8 +1,11 @@
 import time
 import readers
 import logging
+import os
+import requests
 
 logging.basicConfig(filename='Logs/app_timer.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+
 
 def alarm_clock(threadName):
     while True:
@@ -12,13 +15,14 @@ def alarm_clock(threadName):
         print(readers.time_d)
         if time.strftime('%H') == readers.time_d['start']:
             for i in readers.players_d:
-                # wakeonlan by i
-
+                cmd = f'wakeonlan {i}'
+                os.system(cmd)
             time.sleep(120)
+            # time.sleep(10800)
         elif time.strftime('%H') == readers.time_d['end']:
             for i in readers.players_d:
-                # send sleep by readers.players_d[i]
-
+                link = f'http://{readers.players_d[i]}:8080/suspend'
+                requests.get(link)
             time.sleep(120)
-
+            # time.sleep(10800)
         print(time.strftime('%H'), time.strftime('%M'))
---
+++ Installation/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install wakeonlan -y
+
+sudo apt-get install python3.9 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python-selenium python3-selenium -y
+
+sudo pip3 install -r requirements.txt
+sudo pip3 install --upgrade requests
