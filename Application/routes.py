529ab8c5d571badc2ec04611d51133718bda52d2
Damir <nabiull2020@gmail.com>
2022-04-08 21:08:06 +0300

    Updated routes.py

--- Application/routes.py
+++ Application/routes.py
@@ -1,7 +1,8 @@
 from aiohttp import web
 import logging
 import readers
-
+import requests
+import os
 
 logging.basicConfig(filename='Logs/app_routes.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
@@ -28,7 +29,25 @@ async def post_timer(request):
 
         readers.write_time()
         readers.write_players()
+
+        status = 200
     except Exception as e:
         logging.error(f'/set : status - {e}')
     data = {'status': status}
     return web.json_response(status=status, data=data)
+
+
+@routes.get('/suspendAll')
+async def get_ping(request):
+    readers.read_players()
+    for i in readers.players_d:
+        link = f'http://{readers.players_d[i]}:8080/suspend'
+        requests.get(link)
+
+
+@routes.get('/startAll')
+async def get_ping(request):
+    readers.read_players()
+    for i in readers.players_d:
+        cmd = f'wakeonlan {i}'
+        os.system(cmd)
